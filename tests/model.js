ae1a18626f5c1f2b352a9565a7621488addfb976
A mobile-first, OOCSS framework <harmse@morainevalley.edu>
2016-10-12 09:57:09 -0500

    Updated

--- tests/model.js
+++ tests/model.js
@@ -1,6 +1,9 @@
-describe('model', function() {
+describe('Model', function() {
 	it('should init', function() {
 		var People = new leaf.Model();
 		expect(People.on).toBeDefined();
+	});
+	it('should fail', function() {
+		expect(true).toBe(false);
 	});
 });
