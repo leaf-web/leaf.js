3f2f621a746bd5dc5d5285f0a0452bce90c6d708
A mobile-first, OOCSS framework <harmse@morainevalley.edu>
2016-10-12 10:52:39 -0500

    Updated

--- tests/functions.js
+++ tests/functions.js
@@ -14,4 +14,16 @@ describe('Functions', function() {
 	it('should return true when checking an Array with isArray', function() {
 		expect(leaf.isArray([1,2,3])).toBe(true);
 	});
+
+	it('should return true when checking a Function with isFunction', function() {
+		expect(leaf.isFunction(function() {})).toBe(true);
+	});
+
+	it('should return true when checking an Object with isObject', function() {
+		expect(leaf.isObject({})).toBe(true);
+	});
+
+	it('should return true when checking a Date with isDate', function() {
+		expect(leaf.isDate(new Date())).toBe(true);
+	});
 });
