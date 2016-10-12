f8182c1fffb37d9351857f938884ecd4e8ac643d
A mobile-first, OOCSS framework <harmse@morainevalley.edu>
2016-10-12 09:26:38 -0500

    Updated

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'tests/*.js',
+    basePath: '',
 
 
     // frameworks to use
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+    	'tests/model.js'
     ],
 
 
