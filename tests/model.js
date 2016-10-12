2b216c358f886deba7289db12d383e38cd96b7dc
A mobile-first, OOCSS framework <harmse@morainevalley.edu>
2016-10-12 11:25:47 -0500

    Updated

--- tests/model.js
+++ tests/model.js
@@ -8,18 +8,26 @@ describe('Model', function() {
 		expect(leaf.isDefined(Person)).toBe(true);
 	});
 
-	it('should be able to get an attribute.', function() {
+	it('should be able to get an attribute', function() {
 		expect(Person.get('lastName')).toBe('Doe');
 	});
 
-	it('should be able to set an attribute.', function() {
+	it('should be able to set an attribute', function() {
 		Person.set('firstName', 'Jane');
-
 		expect(Person.get('firstName')).toBe('Jane');
 	});
 
-	it('should be able to determine if it was changed.', function() {
+	it('should be able to determine if it was changed', function() {
 		expect(Person.changed).toBe(true);
+	});
+
+	it('should be able to remove an attribute', function() {
+		Person.remove('firstName');
+		expect(Person.contains('firstName')).toBe(false);
+	});
+
+	it('should be able to check if an attribute exists', function() {
+		expect(Person.contains('firstName')).toBe(false);
 	});
 });
 
